88869582eb82ebff10d9d82c619ec87f1adc4055
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-02-18 21:01:00 -0600

    Create User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user) }
+  subject { user }
+
+  context 'validations' do
+  	it { should validate_presence_of(:email) }
+  	it { should validate_presence_of(:username) }
+  	it { should validate_presence_of(:password) }
+  	it { should be_valid }
+  end
+
+  context 'associations' do
+  	it { should belong_to(:membership) }
+
+  	it { should have_many(:payments) }
+  	it { should have_many(:objectives) }
+  	it { should have_many(:attendances) }
+  	it { should have_many(:physical_tests) }
+  end
 end
