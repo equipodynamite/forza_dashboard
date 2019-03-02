a7e4195fda359d58fae143f0ab5d1b70f73cd7ae
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-03-02 13:02:54 -0600

    Remove password presence validation User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe User, type: :model do
   context 'validations' do
   	it { should validate_presence_of(:email) }
   	it { should validate_presence_of(:username) }
-  	it { should validate_presence_of(:password) }
   	# it { should be_valid }
   end
 
