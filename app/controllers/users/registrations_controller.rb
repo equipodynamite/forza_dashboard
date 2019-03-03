9b31dc5adb85b0873282742bcf9aded4b4040213
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-03-02 18:35:34 -0600

    Create Users::RegistrationsController and add it to routes.rb file for updating user process

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def update
+    super do |resource|
+      if params[:user][:password].blank?
+        current_user.update(user_params_no_password)
+      else
+        current_user.update(user_params)
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def user_params_no_password
+    params.require(:user).permit(:username, :email)
+  end
+end
