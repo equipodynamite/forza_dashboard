89990b0687befefaf052394a0bc8ebf1db62ab22
Hector Mg <hectormgwake@gmail.com>
2019-03-03 20:33:31 -0600

    Add login view as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
+  root "users#sign_in"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
