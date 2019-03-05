b7fef574dd4a5cfc435f22179d42ad3cfdda0f72
Hector Mg <hectormgwake@gmail.com>
2019-03-04 18:30:47 -0600

    Route authenticated and unauthenticated users to respectice root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root "users#sign_in"
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  devise_scope :user do
+    authenticated :user do
+      root to: "attendances#index", as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
   resources :attendances, only: [:index]
 end
