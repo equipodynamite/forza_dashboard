54bc276d4e44ec18f740ed437ed715de1dc02041
Hector Mg <hectormgwake@gmail.com>
2019-03-15 15:10:16 -0600

    Update application seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-u = User.create(username: "Example",
-                email: FFaker::Internet.email,
-                password: '123456',
-                password_confirmation: '123456')
+member = User.create(username: "Member",
+                    email: FFaker::Internet.email,
+                    password: '123456',
+                    password_confirmation: '123456')
+member.add_role :member                    
+admin = User.create(username: "Admin",
+                    email: FFaker::Internet.email,
+                    password: '123456',
+                    password_confirmation: '123456')
+admin.add_role :admin
