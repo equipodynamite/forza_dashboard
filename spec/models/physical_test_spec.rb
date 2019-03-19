1a475d596875107c15d49becce7ebe453cd91ca5
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-03-19 00:52:32 -0600

    Add specs for physical tests

--- spec/models/physical_test_spec.rb
+++ spec/models/physical_test_spec.rb
@@ -5,7 +5,27 @@ RSpec.describe PhysicalTest, type: :model do
   subject { physical_test }
 
   context 'validations' do
-  	# it { should be_valid }
+		it { should validate_presence_of(:suggested_program) }
+		it { should validate_presence_of(:squats) }
+		it { should validate_presence_of(:pull_ups) }
+		it { should validate_presence_of(:push_ups) }
+		it { should validate_presence_of(:crunches) }
+		it { should validate_presence_of(:squats_rom) }
+		it { should validate_presence_of(:pull_ups_rom) }
+		it { should validate_presence_of(:push_ups_rom) }
+		it { should validate_presence_of(:crunches_rom) }
+		it { should validate_presence_of(:squats_form) }
+		it { should validate_presence_of(:pull_ups_form) }
+		it { should validate_presence_of(:push_ups_form) }
+		it { should validate_presence_of(:crunches_form) }
+		it { should validate_presence_of(:squats_core) }
+		it { should validate_presence_of(:pull_ups_core) }
+		it { should validate_presence_of(:push_ups_core) }
+		it { should validate_presence_of(:crunches_core) }
+		it { should validate_presence_of(:squats_rhythm) }
+		it { should validate_presence_of(:pull_ups_rhythm) }
+		it { should validate_presence_of(:push_ups_rhythm) }
+		it { should validate_presence_of(:crunches_rhythm) }
   end
 
   context 'associations' do
