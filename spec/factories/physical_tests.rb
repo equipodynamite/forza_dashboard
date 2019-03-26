b0b17a0006d743a7f71b92b1a8563851153dc220
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-03-26 17:47:57 -0600

    Resolve indentation and end line details on files

--- spec/factories/physical_tests.rb
+++ spec/factories/physical_tests.rb
@@ -1,27 +1,27 @@
 FactoryBot.define do
   factory :physical_test do
-    date 							{ FFaker::Time.datetime }
-    user_id						{ 12 }
+    date              { FFaker::Time.datetime }
+    user_id           { 12 }
     suggested_program { rand(11) }
-		squats						{ rand(100) }
-		pull_ups					{ rand(100) }
-		push_ups					{ rand(100) }
-		crunches					{ rand(100) }
-		squats_rom				{ rand(1) }
-		pull_ups_rom			{ rand(1) }
-		push_ups_rom			{ rand(1) }
-		crunches_rom			{ rand(1) }
-		squats_form				{ rand(1) }
-		pull_ups_form			{ rand(1) }
-		push_ups_form			{ rand(1) }
-		crunches_form			{ rand(1) }
-		squats_core				{ rand(1) }
-		pull_ups_core			{ rand(1) }
-		push_ups_core			{ rand(1) }
-		crunches_core			{ rand(1) }
-		squats_rhythm			{ rand(2) }
-		pull_ups_rhythm		{ rand(2) }
-		push_ups_rhythm		{ rand(2) }
-		crunches_rhythm		{ rand(2) }
+    squats            { rand(100) }
+    pull_ups          { rand(100) }
+    push_ups          { rand(100) }
+    crunches          { rand(100) }
+    squats_rom        { rand(1) }
+    pull_ups_rom      { rand(1) }
+    push_ups_rom      { rand(1) }
+    crunches_rom      { rand(1) }
+    squats_form       { rand(1) }
+    pull_ups_form     { rand(1) }
+    push_ups_form     { rand(1) }
+    crunches_form     { rand(1) }
+    squats_core       { rand(1) }
+    pull_ups_core     { rand(1) }
+    push_ups_core     { rand(1) }
+    crunches_core     { rand(1) }
+    squats_rhythm     { rand(2) }
+    pull_ups_rhythm   { rand(2) }
+    push_ups_rhythm   { rand(2) }
+    crunches_rhythm   { rand(2) }
   end
 end
